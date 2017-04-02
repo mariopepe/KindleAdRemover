755a1e5aa63d1c6883b8c62e66bc783dc6a5d9ec
peps96 <96.peps@gmail.com>
2017-04-02 15:46:06 +0200

    Loaded Application Package and script

---
+++ KindleAdRemover.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+sleep 2
+echo 
+echo "    ________________________________________________________________    ";
+echo "   |                                    _                           |   ";
+echo "   |   |/ • ._   _| |  _     /\   _|   |_)  _  ._ _   _      _  ._  |   ";
+echo "   |   |\ | | | (_| | (/_   /--\ (_|   | \ (/_ | | | (_) \/ (/_ |   |   ";
+echo "   |                                                                |   ";
+echo "    ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯ ";
+echo
+echo "                     Brought to you by scugn1zz0";
+echo
+echo
+echo
+echo " This simple script removes Special Offers from your Kindle ";
+echo
+sleep 1
+echo " NOTE: every time you update you need to redo this procedure"
+echo
+echo
+
+sleep 2
+
+read -p "You want to continue[y/n]? " -n 1 -r
+echo    
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	sleep 2
+	
+	{rm -r /Volumes/Kindle/system/.assets} &> /dev/null
+	if [ $? -eq 0 ]; then
+		echo	
+	else
+    	echo "not working..."
+	fi
+
+	echo "wait a moment...";
+	sleep 4
+
+	{touch /Volumes/Kindle/system/.assets} &> /dev/null
+	if [ $? -eq 0 ]; then
+		ech89
+		echo "done"
+	else
+    	echo "not..working"
+	fi
+fi
+
