61e6143d7f5dca96484b1587740346020d44d147
peps96 <scugnzz@gmail.com>
2017-04-02 20:18:22 +0200

    Fixed little bugs

--- KindleAdRemover.sh
+++ KindleAdRemover.sh
@@ -27,22 +27,24 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	sleep 2
 	
-	{rm -r /Volumes/Kindle/system/.assets} &> /dev/null
+	rm -r /Volumes/Kindle/system/.assets
 	if [ $? -eq 0 ]; then
 		echo	
 	else
     	echo "not working..."
+    	echo "your kindle may be not supported, or just it is not recognised"
 	fi
 
 	echo "wait a moment...";
 	sleep 4
 
-	{touch /Volumes/Kindle/system/.assets} &> /dev/null
+	touch /Volumes/Kindle/system/.assets
 	if [ $? -eq 0 ]; then
 		ech89
 		echo "done"
 	else
     	echo "not..working"
+    	echo "your kindle may be not supported, or just it is not recognised"
 	fi
 fi
 
